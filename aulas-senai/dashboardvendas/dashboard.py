7ca63f7e30b480e3bca2f2d83284dce112ae21d1
thiago <thi.henriquemaia@gmail.com>
2024-10-08 14:43:38 -0300

    projeto

---
+++ aulas-senai/dashboardvendas/dashboard.py
@@ -0,0 +1,13 @@
+import streamlit as st
+import requests
+import pandas as pd
+import plotly.express as px
+
+st.title('Dahboard de vendas: shopping_trolley')
+
+
+url ='https://labdados.com/produtos'
+
+response= requests.get(url)
+dados= pd.DataFrame.from_dict(response.json())
+st.dataframe(dados)
